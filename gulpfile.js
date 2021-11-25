333301048077cb826ffcf672b982d4f1321e7ac0
ccuu-p <cat.can.use.unix+mbp@gmail.com>
2021-11-26 01:47:21 +0900

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
-var gulp    = require('gulp'),
-    sass    = require('gulp-sass')(require('sass')),
-    connect = require('gulp-connect'),
-    plumber = require('gulp-plumber'),
-    rename  = require('gulp-rename'),
-    uglify  = require('gulp-uglify'),
-    autoprefixer = require('gulp-autoprefixer'),
-    sourcemaps = require('gulp-sourcemaps'),
-    changed = require('gulp-changed'),
-    cached = require('gulp-cached'),
-    gulpejs = require("gulp-ejs");
+var gulp          = require('gulp'),
+    sass          = require('gulp-sass')(require('sass')),
+    connect       = require('gulp-connect'),
+    plumber       = require('gulp-plumber'),
+    notify        = require('gulp-notify'),
+    rename        = require('gulp-rename'),
+    uglify        = require('gulp-uglify'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    changed       = require('gulp-changed'),
+    cached        = require('gulp-cached'),
+    gulpejs       = require('gulp-ejs'),
+    htmlbeautify  = require('gulp-html-beautify'),
+    eslint        = require('gulp-eslint'),
+    scsslint      = require('gulp-scss-lint');
 
-
-var htmlbeautify = require("gulp-html-beautify");
-var path = require('path');
-var fs = require('fs');
+var path          = require('path');
+var fs            = require('fs');
 
 
 // Input Directory
@@ -27,7 +29,7 @@ var srcPath = {
 
 // Output Directory
 var destPath = {
-  'root': './dist',
+  'root': './dist/',
   'html': './dist',
   'ejs': './dist',
   'css': './dist/assets/css',
@@ -38,6 +40,7 @@ var destPath = {
 
 function reload(done) {
   connect.server({
+    root: destPath.root,
     livereload: true,
     port: 8080
   });
@@ -45,19 +48,22 @@ function reload(done) {
 }
 
 function html() {
-  return (
-    gulp.src('./dest/*.html')
+  return gulp.src('./dest/*.html')
     .pipe(plumber())
-    .pipe(connect.reload())
-  );
+    .pipe(connect.reload());
 }
 
 function styles() {
-  return (
-    gulp.src(srcPath.sass)
+  return gulp.src(srcPath.sass)
     .pipe(sourcemaps.init())
-    .pipe(cached('sass'))
-    .pipe(plumber())
+    // .pipe(cached('sass'))
+    .pipe(scsslint({ 'maxBuffer': 10000 }) )
+    .pipe(plumber({
+      errorHandler: notify.onError({
+        message: 'Error: <%= error.message %>',
+        title: 'styles'
+      })
+    }))
     .pipe(sass())
     .pipe(autoprefixer({
       overrideBrowserslist: ['last 3 versions'],
@@ -65,60 +71,65 @@ function styles() {
     }))
     .pipe(sass({outputStyle: 'expanded'}))
     .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(destPath.css))
-    // .pipe(sass({outputStyle: 'compressed'}))
-    // .pipe(rename('styles.min.css'))
-    // .pipe(sourcemaps.write('./'))
-    // .pipe(gulp.dest(destPath.css))
-    .pipe(connect.reload())
-  );
+    .pipe(gulp.dest(destPath.css));
 }
+
 
 function scripts() {
-  return (
-    gulp.src(srcPath.js)
-      .pipe(cached('scripts'))
-      .pipe(plumber())
-      .pipe(gulp.dest(destPath.js))
-      .pipe(uglify())
-      .pipe(rename({extname: '.min.js'}))
-      .pipe(gulp.dest(destPath.js))
-      .pipe(connect.reload())
-  );
+  return gulp.src(srcPath.js)
+    .pipe(cached('scripts'))
+    .pipe(plumber({
+      errorHandler: notify.onError({
+        message: 'Error: <%= error.message %>',
+        title: 'scripts'
+      })
+    }))
+    .pipe(gulp.dest(destPath.js))
+    .pipe(uglify())
+    .pipe(rename({extname: '.min.js'}))
+    .pipe(gulp.dest(destPath.js));
 }
 
-// gulp.task('ejs', function(done) {
-//   return gulp.src(srcPath.ejs)
-//     // .pipe(cached('ejs'))
-//     .pipe(changed('ejs'))
-//     .pipe(plumber())
-//     .pipe(ejs({title: 'gulp-ejs'}))
-//     .pipe(
-//       htmlbeautify({
-//         indent_size: 2, //インデントサイズ
-//         indent_char: " ", // インデントに使う文字列はスペース1こ
-//         max_preserve_newlines: 0, // 許容する連続改行数
-//         preserve_newlines: false, //コンパイル前のコードの改行
-//         indent_inner_html: false, //head,bodyをインデント
-//         extra_liners: [], // 終了タグの前に改行を入れるタグ。配列で指定。head,body,htmlにはデフォで改行を入れたくない場合は[]。
-//       })
-//     )
-//     .pipe(rename({ extname: '.html' }))
-//     .pipe(gulp.dest(destPath.ejs));
-//   done();
-// });
+function checkJs() {
+  return gulp.src(srcPath.js) // lint のチェック先を指定
+    .pipe(plumber({
+      // エラーをハンドル
+      errorHandler: function(error) {
+        var taskName = 'eslint';
+        var title = '[task]' + taskName + ' ' + error.plugin;
+        var errorMsg = 'error: ' + error.message;
+        // ターミナルにエラーを出力
+        console.error(title + '\n' + errorMsg);
+        // エラーを通知
+        notify.notify({
+          title: title,
+          message: errorMsg,
+          time: 3000
+        });
+      }
+    }))
+    .pipe(eslint({ useEslintrc: true })) // .eslintrc を参照
+    .pipe(eslint.format())
+    .pipe(eslint.failOnError())
+    .pipe(plumber.stop());
+}
 
 
-function ejs() { 
+function ejs() {
   return gulp.src(srcPath.ejs)
     .pipe(changed('ejs'))
-    .pipe(plumber())
+    .pipe(plumber({
+      errorHandler: notify.onError({
+        message: 'Error: <%= error.message %>',
+        title: 'ejs'
+      })
+    }))
     .pipe(gulpejs({ title: 'gulp-ejs' }))
     .pipe(
       htmlbeautify({
-        indent_size: 2, 
-        indent_char: " ", 
-        max_preserve_newlines: 0, 
+        indent_size: 2,
+        indent_char: ' ',
+        max_preserve_newlines: 0,
         preserve_newlines: false, //コンパイル前のコードの改行
         indent_inner_html: false, //head,bodyをインデント
         extra_liners: [], // 終了タグの前に改行を入れるタグ。配列で指定。head,body,htmlにはデフォで改行を入れたくない場合は[]。
@@ -128,38 +139,27 @@ function ejs() {
     .pipe(gulp.dest(destPath.ejs));
 }
 
-// function compileEjs() { 
-//   return gulp.src(srcPath.ejs)
-//     .pipe(ejs({ title: 'gulp-ejs' }))
-//     .pipe(rename({ extname: '.html' }))
-//     .pipe(gulp.dest(destPath.ejs));
-// }
-
 
 // gulp.watch(ファイル, 処理)
 function watchTask(done) {
-  // gulp.watch('*.html', html);
+  gulp.watch('*.html', html);
   gulp.watch('src/sass/**/*.scss', styles);
   gulp.watch('src/js/**/*.js', scripts);
   gulp.watch(srcPath.ejs, ejs);
-  // gulp.watch(srcPath.ejs, gulp.task("ejs"));
-  
+  gulp.watch(srcPath.js, checkJs);
   done();
 }
 
 
 var watch = gulp.parallel(watchTask, reload);
-// var build = gulp.series(gulp.parallel(styles, scripts, gulp.task("ejs")));
-// var build = gulp.series(gulp.parallel(styles, scripts, compileEjs));
 var build = gulp.series(gulp.parallel(styles, scripts, ejs));
 
 exports.reload = reload;
-// exports.compile = compileEjs;
+exports.html = html;
 exports.ejs = ejs;
+exports.checkJs = checkJs;
 exports.styles = styles;
 exports.scripts = scripts;
-exports.html = html;
-exports.watch = watch;
 exports.build = build;
+exports.watch = watch;
 exports.default = watch;
-
